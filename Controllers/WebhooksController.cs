618042ac202c89b5f6c462974960ff108cc0a795
Ruben Pacheco <ruben.pacheco.lopez@gmail.com>
2024-05-28 23:59:22 -0600

    Initial commit

---
+++ Controllers/WebhooksController.cs
@@ -0,0 +1,5 @@
+namespace EjemploWebhookBioengine.Controllers;
+
+public class WebhooksController{
+    
+}
---
+++ Models/MatchDto.cs
@@ -0,0 +1,5 @@
+namespace EjemploWebhookBioengine.Models;
+
+public class MatchDto{
+    
+}
